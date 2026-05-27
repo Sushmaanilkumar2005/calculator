fa94fde287bb824cc469053b9e77490cb296d809
Sushmaanilkumar2005 <sushmachinkera0@gmail.com>
2026-05-27 19:59:15 +0530

    first commit

---
+++ task5/string.py
@@ -0,0 +1,11 @@
+'''The included code stub will read an integer, , from STDIN.
+Without using any string methods, try to print the following:
+Note that "" represents the consecutive values in between.
+
+Example
+n = 5
+Print the string 1234'''
+
+n = int(input())
+for i in range(1,n+1):
+    print(i, end="")
